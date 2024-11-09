17119d8a61e864c916176064aff1e28a327b59a8
Mykhailo Vasylenko <mishamoovex@gmail.com>
2024-11-09 16:28:47 +0200

    Extracted common microservices configurations

--- auth-service/build.gradle.kts
+++ auth-service/build.gradle.kts
@@ -3,18 +3,7 @@ plugins {
     java
 }
 
-group = "com.example"
-version = "0.0.1-SNAPSHOT"
-
-repositories {
-    mavenCentral()
-}
-
 dependencies {
     implementation(libs.spring.starter)
     implementation(libs.spring.web)
-}
-
-tasks.withType<Test> {
-    useJUnitPlatform()
 }
---
+++ build.gradle.kts
@@ -0,0 +1,12 @@
+subprojects {
+    group = "com.example"
+    version = "0.0.1-SNAPSHOT"
+
+    repositories {
+        mavenCentral()
+    }
+
+    tasks.withType<Test> {
+        useJUnitPlatform()
+    }
+}
