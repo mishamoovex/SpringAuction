fdd1a9a7445a45aea78294ddee1345e00744b454
Mykhailo Vasylenko <mishamoovex@gmail.com>
2024-11-09 20:58:59 +0200

    Implemented UserEntity

--- user-service/src/main/java/com/lead/service/user/repository/entity/UserEntity.java
+++ user-service/src/main/java/com/lead/service/user/repository/entity/UserEntity.java
@@ -1,9 +1,6 @@
 package com.lead.service.user.repository.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.Data;
 
 @Data
@@ -12,7 +9,10 @@ public class UserEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
     private String id;
+    @Column(nullable = false, length = 30)
     private String firstName;
+    @Column(nullable = false, length = 30)
     private String lastName;
+    @Column(length = 40)
     private String email;
 }
