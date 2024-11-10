153ae44f5ef1d57dfb53dec0a777b05e7678d05c
Mykhailo Vasylenko <mishamoovex@gmail.com>
2024-11-10 22:51:09 +0200

    Fixed timestamp mapping in test matcher

--- user-service/build.gradle.kts
+++ user-service/build.gradle.kts
@@ -6,7 +6,6 @@ plugins {
 dependencies {
     implementation(project(":core"))
 
-    implementation(libs.java.time)
     implementation(libs.bundles.data.service)
     compileOnly(libs.lombok)
     annotationProcessor(libs.lombok)
--- user-service/src/test/java/com/lead/service/util/matchers/ResponseBodyMatchers.java
+++ user-service/src/test/java/com/lead/service/util/matchers/ResponseBodyMatchers.java
@@ -1,12 +1,20 @@
 package com.lead.service.util.matchers;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import org.springframework.test.web.servlet.ResultMatcher;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class ResponseBodyMatchers {
-    private final ObjectMapper objectMapper = new ObjectMapper();
+    private final ObjectMapper objectMapper;
+
+    private ResponseBodyMatchers() {
+        ObjectMapper timestampMapper = new ObjectMapper();
+        timestampMapper.registerModule(new JavaTimeModule());
+        this.objectMapper = timestampMapper;
+    }
 
     public <T> ResultMatcher containsObjectAsJson(
             Object expectedObject,
