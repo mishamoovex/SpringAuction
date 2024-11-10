a1ecf974c463723b0afdc623516769b44af662ff
Mykhailo Vasylenko <mishamoovex@gmail.com>
2024-11-10 11:45:39 +0200

    Added all arguments constructor to the request DTO

--- user-service/src/main/java/com/lead/service/user/controller/dto/RegisterRequestDTO.java
+++ user-service/src/main/java/com/lead/service/user/controller/dto/RegisterRequestDTO.java
@@ -1,8 +1,10 @@
 package com.lead.service.user.controller.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Data
+@AllArgsConstructor
 public class RegisterRequestDTO {
     private String firstName;
     private String lastName;
