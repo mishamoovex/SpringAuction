0788f31be9ffa50de79b8beb1f668f598e657acc
Mykhailo Vasylenko <mishamoovex@gmail.com>
2024-11-10 21:42:32 +0200

    Added java time support

--- user-service/build.gradle.kts
+++ user-service/build.gradle.kts
@@ -6,9 +6,11 @@ plugins {
 dependencies {
     implementation(project(":core"))
 
+    implementation(libs.java.time)
     implementation(libs.bundles.data.service)
     compileOnly(libs.lombok)
     annotationProcessor(libs.lombok)
 
     testImplementation(libs.bundles.unit.testing)
+    testImplementation(libs.java.time)
 }
