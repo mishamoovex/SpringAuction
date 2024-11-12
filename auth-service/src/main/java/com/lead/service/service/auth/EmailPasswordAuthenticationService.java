d1e3138af4e1a74f304d02d962c1b6d96190928b
Mykhailo Vasylenko <mishamoovex@gmail.com>
2024-11-12 22:18:45 +0200

    User service client implementation

--- auth-service/src/main/java/com/lead/service/service/auth/EmailPasswordAuthenticationService.java
+++ auth-service/src/main/java/com/lead/service/service/auth/EmailPasswordAuthenticationService.java
@@ -5,12 +5,15 @@ import com.lead.service.model.request.LoginRequest;
 import com.lead.service.model.request.RegistrationRequest;
 import com.lead.service.model.request.TokenRequest;
 import com.lead.service.model.response.AuthResponse;
+import com.lead.service.web.UserServiceClient;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
 @AllArgsConstructor
 public class EmailPasswordAuthenticationService implements AuthenticationService {
+
+    private UserServiceClient userServiceClient;
 
     @Override
     public AuthResponse login(LoginRequest loginRequest) {
---
+++ auth-service/src/main/java/com/lead/service/web/UserServiceClient.java
@@ -0,0 +1,23 @@
+package com.lead.service.web;
+
+import com.lead.service.model.dto.UserDto;
+import com.lead.service.model.request.RegistrationRequest;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.Email;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+@FeignClient(name = "user-service", url = "http://localhost:8082/v1/user")
+@Validated
+public interface UserServiceClient {
+
+    @PostMapping("/save")
+    UserDto save(@RequestBody @Valid RegistrationRequest request);
+
+    @GetMapping("/findByEmail/{email}")
+    UserDto getByEmail(@PathVariable @Email String email);
+}
