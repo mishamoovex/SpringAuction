76b6cf5133043bbf8c20451c4143413cdb19c5c9
Mykhailo Vasylenko <mishamoovex@gmail.com>
2024-11-12 23:06:09 +0200

    AuthService registration implementation

--- auth-service/src/main/java/com/lead/service/service/auth/EmailPasswordAuthenticationService.java
+++ auth-service/src/main/java/com/lead/service/service/auth/EmailPasswordAuthenticationService.java
@@ -5,9 +5,10 @@ import com.lead.service.model.request.LoginRequest;
 import com.lead.service.model.request.RegistrationRequest;
 import com.lead.service.model.request.TokenRequest;
 import com.lead.service.model.response.AuthResponse;
+import com.lead.service.service.token.TokenService;
 import com.lead.service.web.UserServiceClient;
 import lombok.AllArgsConstructor;
-import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -15,15 +16,22 @@ import org.springframework.stereotype.Service;
 public class EmailPasswordAuthenticationService implements AuthenticationService {
 
     private UserServiceClient userServiceClient;
-    private PasswordEncoder passwordEncoder;
+    private TokenService tokenService;
+    private AuthenticationManager authenticationManager;
 
+    //TODO do I need to call authenticate after registration???
     @Override
-    public AuthResponse login(LoginRequest loginRequest) {
-        return null;
+    public AuthResponse register(RegistrationRequest registrationRequest) {
+        var user = userServiceClient.save(registrationRequest);
+        var token = new TokenDto(
+                tokenService.generateAccessToken(user.getEmail()),
+                tokenService.generateRefreshToken(user.getEmail())
+        );
+        return new AuthResponse(user, token);
     }
 
     @Override
-    public AuthResponse register(RegistrationRequest registrationRequest) {
+    public AuthResponse login(LoginRequest loginRequest) {
         return null;
     }
 
