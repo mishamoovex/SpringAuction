1932a43a7ff6fa5a60f393e95d404df1abeda7f9
Mykhailo Vasylenko <mishamoovex@gmail.com>
2024-11-15 23:29:16 +0200

    Updated Feign clients to use serverName instead of URL

--- service/auth/src/main/java/com/lead/service/auth/web/UserServiceClient.java
+++ service/auth/src/main/java/com/lead/service/auth/web/UserServiceClient.java
@@ -6,7 +6,11 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
-@FeignClient(name = "user-service", url = "http://localhost:8082/v1/user")
+@FeignClient(
+        name = "user-service",
+        contextId = "user-service-authentication",
+        path = "/v1/user"
+)
 public interface UserServiceClient {
 
     @PostMapping
