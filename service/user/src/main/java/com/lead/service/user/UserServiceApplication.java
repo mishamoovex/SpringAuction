6c90688fb01fcd550029d7ccb14e0753c0213670
Mykhailo Vasylenko <mishamoovex@gmail.com>
2024-11-16 15:42:27 +0200

    Enabled Spring method authorization

--- service/user/src/main/java/com/lead/service/user/UserServiceApplication.java
+++ service/user/src/main/java/com/lead/service/user/UserServiceApplication.java
@@ -3,9 +3,11 @@ package com.lead.service.user;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 
 @SpringBootApplication
 @EnableFeignClients
+@EnableMethodSecurity
 public class UserServiceApplication {
 
     public static void main(String[] args) {
