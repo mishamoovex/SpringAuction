f264e0ccbf46dffa2c182342d8f3071e5abb3fec
Mykhailo Vasylenko <mishamoovex@gmail.com>
2024-11-16 14:28:03 +0200

    Configured API Gateway (#14)
    * Gateway service set up
    
    * Set up basic gateway routing

---
+++ gateway/build.gradle.kts
@@ -0,0 +1,10 @@
+plugins {
+    alias(libs.plugins.spring)
+    alias(libs.plugins.spring.dependency)
+    java
+}
+
+dependencies {
+    implementation(platform(libs.spring.cloud.bom))
+    implementation(libs.bundles.gateway)
+}
---
+++ gateway/src/main/java/com/lead/gateway/config/GatewayConfig.java
@@ -0,0 +1,27 @@
+package com.lead.gateway.config;
+
+import org.springframework.cloud.gateway.route.Route;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.Buildable;
+import org.springframework.cloud.gateway.route.builder.PredicateSpec;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.function.Function;
+
+@Configuration
+public class GatewayConfig {
+
+    @Bean
+    public RouteLocator routeLocator(RouteLocatorBuilder builder) {
+        return builder.routes()
+                .route("auth-service", eurekaRoute("/v1/auth/**", "auth-service"))
+                .route("user-service", eurekaRoute("/v1/user/**", "user-service"))
+                .build();
+    }
+
+    private Function<PredicateSpec, Buildable<Route>> eurekaRoute(String path, String serverName) {
+        return r -> r.path(path).uri("lb://" + serverName);
+    }
+}
