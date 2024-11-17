8796f2e9073ce76ea84d4328dd20e3f6265dfa97
Mykhailo Vasylenko <mishamoovex@gmail.com>
2024-11-17 19:57:24 +0200

    Fixed Auction service default bean name

--- service/auction/src/main/java/com/lead/service/auction/service/auction/AuctionServiceImpl.java
+++ service/auction/src/main/java/com/lead/service/auction/service/auction/AuctionServiceImpl.java
@@ -15,7 +15,7 @@ import org.springframework.transaction.annotation.Transactional;
 import java.time.Clock;
 import java.time.LocalDateTime;
 
-@Service
+@Service("auctionService")
 @AllArgsConstructor
 public class AuctionServiceImpl implements AuctionService {
 
