bd78ecd564bf836cee724d94313b6bdb39427327
Mykhailo Vasylenko <mishamoovex@gmail.com>
2024-11-19 21:07:19 +0200

    Set up lot service

---
+++ service/lot/src/main/java/com/lead/service/lot/LotServiceApplication.java
@@ -0,0 +1,12 @@
+package com.lead.service.lot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LotServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LotServiceApplication.class, args);
+    }
+}
