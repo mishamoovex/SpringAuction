cc52977ec1b09baf64a181b9fb8c2b24e95bf502
Mykhailo Vasylenko <mishamoovex@gmail.com>
2024-11-20 13:43:33 +0200

    Get methods controller implementation

--- service/lot/src/main/java/com/lead/service/lot/controller/LotController.java
+++ service/lot/src/main/java/com/lead/service/lot/controller/LotController.java
@@ -9,12 +9,16 @@ import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/v1/lot")
@@ -33,6 +37,16 @@ public class LotController {
     @PreAuthorize("@authorizationService.isLotAdmin(#request.lotId, authentication.principal.id)")
     public ResponseEntity<LotDto> update(@RequestBody @Valid UpdateLotRequest request) {
         return ResponseEntity.ok(lotService.update(request));
+    }
+
+    @GetMapping("/{lotId}")
+    public ResponseEntity<LotDto> getById(@PathVariable String lotId) {
+        return ResponseEntity.ok(lotService.getById(lotId));
+    }
+
+    @GetMapping("/getAll")
+    public ResponseEntity<List<LotDto>> getAllByAuction(@RequestParam String auctionId) {
+        return ResponseEntity.ok(lotService.getAllByAuction(auctionId));
     }
 
     @DeleteMapping("/{lotId}")
