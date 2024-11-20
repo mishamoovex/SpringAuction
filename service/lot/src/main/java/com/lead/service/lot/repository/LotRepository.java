e6be6ad89e60120f51a59029b2a41d761eaa600e
Mykhailo Vasylenko <mishamoovex@gmail.com>
2024-11-20 13:44:22 +0200

    GET lot service methods (#41)
    * Find lots by auction repository
    
    * Find all service implementation
    
    * Get methods controller implementation

--- service/lot/src/main/java/com/lead/service/lot/repository/LotRepository.java
+++ service/lot/src/main/java/com/lead/service/lot/repository/LotRepository.java
@@ -3,5 +3,9 @@ package com.lead.service.lot.repository;
 import com.lead.service.lot.models.entity.LotEntity;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 public interface LotRepository extends CrudRepository<LotEntity, String> {
+
+    List<LotEntity> findAllByAuctionId(String auctionId);
 }
